ee7d0af303f9f6664c8a1af07d54659179c9efa5
Alan Yuan <alanyuan_ca@yahoo.ca>
2015-05-28 15:15:36 -0400

    no controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,17 +5,7 @@ class ProductsController < ApplicationController
 	end
 
 	def request_product
-		respond_to do |format|
-			@products = Product.all
 
-			format.html {
-				render :partial => "products/partial/list_partial"
-			}
-			format.json {
-
-				render :json => @products
-			}
-		end
 	end
 
 	def directive
