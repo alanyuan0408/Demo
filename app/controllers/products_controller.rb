d9e46467e4d3db493852ea010e47b6407d342f9c
Alan Yuan <alanyuan_ca@yahoo.ca>
2015-06-01 10:56:05 -0400

    removed controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,29 +14,6 @@ class ProductsController < ApplicationController
 	end
 
 	def request_sum
-
-		if (Product.exists?(name: params[:product_name]))
-			product = Product.find_by_name(params[:product_name])
-			exists = true
-
-			total = 0
-
-			product.sub_products.each do |sub_product|
-				total += sub_product.price
-			end
-		else
-			exists = false
-		end
-
-		respond_to do |format|
-			format.json {
-				if exists
-					render :json => {:total => total}.to_json, :status => 200, :content_type => 'json'
-				else
-					render :nothing => true, :status => 200, :content_type => 'json'
-				end
-			}
-		end
 		
 	end
 
